156cf0ad9f88b87610861f5b897d171bc1c2e8b1
Mohammed Khaja Mohinuddin <161879901+mohammed4721@users.noreply.github.com>
2025-12-11 23:07:03 +0530

    Time: 1 ms (72.64%) | Memory: 56.7 MB (60.74%) - LeetSync

--- 136-single-number/single-number.js
+++ 136-single-number/single-number.js
@@ -2,16 +2,24 @@
  * @param {number[]} nums
  * @return {number}
  */
+// var singleNumber = function(nums) {
+//     for(let i=0; i<nums.length; i++){
+//         let count = 0;
+//         for(let j=0; j<nums.length; j++){
+//             if(nums[i] === nums[j]){
+//                 count++;
+//             }
+//         }
+//         if(count === 1){
+//             return nums[i];
+//         }
+//     }
+// };
+
 var singleNumber = function(nums) {
-    for(let i=0; i<nums.length; i++){
-        let count = 0;
-        for(let j=0; j<nums.length; j++){
-            if(nums[i] === nums[j]){
-                count++;
-            }
-        }
-        if(count === 1){
-            return nums[i];
-        }
+    let result = 0;
+    for (let n of nums) {
+        result ^= n; // XOR cancels out duplicates
     }
+    return result;
 };
