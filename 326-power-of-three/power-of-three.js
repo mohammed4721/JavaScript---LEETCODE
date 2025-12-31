089f0f6c3354432bdf2c7ebd59d62e05ba1aaebb
Mohammed Khaja Mohinuddin <161879901+mohammed4721@users.noreply.github.com>
2025-12-31 10:55:01 +0530

    Time: 17 ms (10.60%) | Memory: 65.3 MB (73.56%) - LeetSync

---
+++ 326-power-of-three/power-of-three.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfThree = function(n) {
+    while(n>=3){
+        if(n % 3 != 0){
+            return false;
+        }
+
+        n = Math.floor(n/3);
+    }
+
+    return n==1;
+};
