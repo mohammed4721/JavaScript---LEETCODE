e8aa81c37cbc8477f9ce7501c5c05e6a626a2dd9
Mohammed Khaja Mohinuddin <161879901+mohammed4721@users.noreply.github.com>
2025-12-31 10:58:06 +0530

    Time: 0 ms (100.00%) | Memory: 56 MB (42.92%) - LeetSync

---
+++ 342-power-of-four/power-of-four.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfFour = function(n) {
+    while(n>= 4){
+        if(n%4 != 0){
+            return false;
+        }
+        n= Math.floor(n/4);
+    }
+    return n==1;
+};
