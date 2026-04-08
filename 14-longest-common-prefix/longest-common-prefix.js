7d2808050376869f2e919179ba2f0cc6aa157a04
Mohammed Khaja Mohinuddin <161879901+mohammed4721@users.noreply.github.com>
2026-04-08 23:52:59 +0530

    Time: 0 ms (100.00%) | Memory: 51.7 MB (99.90%) - LeetSync

---
+++ 14-longest-common-prefix/longest-common-prefix.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+     if (strs.length === 0) return "";
+
+    let prefix = strs[0]; // take first word
+
+    for (let i = 1; i < strs.length; i++) {
+        while (strs[i].indexOf(prefix) !== 0) {
+            prefix = prefix.slice(0, -1); // remove last char
+
+            if (prefix === "") return "";
+        }
+    }
+
+    return prefix;
+};
