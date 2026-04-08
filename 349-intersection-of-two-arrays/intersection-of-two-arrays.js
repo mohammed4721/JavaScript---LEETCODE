90f852cfa33a4eb4c565d6f287f8cf7e4974df0b
Mohammed Khaja Mohinuddin <161879901+mohammed4721@users.noreply.github.com>
2026-04-09 00:11:24 +0530

    Time: 5 ms (12.66%) | Memory: 56.7 MB (14.98%) - LeetSync

---
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersection = function(nums1, nums2) {
+    let res = []
+    let mySet1 = new Set(nums1);
+    let mySet2 = new Set(nums2);
+    // let unique = [...mySet]
+    console.log(mySet2)
+    for(let num of mySet1){
+        if(mySet2.has(num)){
+            res.push(num)
+        }
+    }
+        return res
+    
+};
